16b5e6df9c4c030b5dfbeab1e7c22cfcccd94742
yawanokye <aadam@ucc.edu.gh>
2026-09-03 22:14:24 +0000

    Delete src/app/app directory

--- src/app/app/admin-signin/page.tsx
+++
@@ -1,5 +0,0 @@
-import AuthForm from "@/app/signin-with-chatgpt/auth-form";
-
-export default function AdminSignInPage() {
-  return <AuthForm portal="admin" returnTo="/?portal=admin" />;
-}
--- src/app/app/student-signin/page.tsx
+++
@@ -1,5 +0,0 @@
-import AuthForm from "@/app/signin-with-chatgpt/auth-form";
-
-export default function StudentSignInPage() {
-  return <AuthForm portal="learner" returnTo="/?portal=learner" />;
-}
